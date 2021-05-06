d918f208193c103e33bbde60c15dca01e6cff84d
RapRy <bigbosstitle@gmail.com>
2021-05-05 20:04:04 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-function App() {
+import BaseComp from './components/BaseComp'
+
+const App = () => {
   return (
     <div>
-      Hello
+      <BaseComp />
     </div>
   );
 }
---
+++ src/components/BaseComp.js
@@ -0,0 +1,9 @@
+const BaseComp = (props) => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+}
+
+export default BaseComp;
