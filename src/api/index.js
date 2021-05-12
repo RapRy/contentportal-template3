88aebd229b6e74f89a6d7ba95e1208a76c16b324
Ryan <ry.quickcell@gmail.com>
2021-05-12 11:51:02 +0800

    update api file

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = "http://localhost:5000";
+const url = "https://port-template-2.herokuapp.com";
 
 export const fetchCategories = (cat) => axios.get(`${url}/categories/${cat}`)
 export const fetchContents = (subcat, cat) => axios.get(`${url}/contents/${cat}/${subcat}`)
